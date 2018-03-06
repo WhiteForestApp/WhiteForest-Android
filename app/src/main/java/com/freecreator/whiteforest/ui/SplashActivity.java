5bd1741c139dd98e9be4a7eed18e6850feb8ab5b
JackYanx <1085473804@qq.com>
2018-03-06 12:47:11 +0800

    Create New Project

---
+++ app/src/main/java/com/freecreator/whiteforest/ui/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.freecreator.whiteforest.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.freecreator.whiteforest.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+}
