405dfa1aa0bbee90bdeb419a20c54b8723fc6a2b
Cute Niko <Cute Niko>
2018-03-10 18:20:09 +0800

    页面自适应

---
+++ app/src/main/java/com/freecreator/whiteforest/ui/utils/AdjustSize.java
@@ -0,0 +1,79 @@
+package com.freecreator.whiteforest.ui.utils;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by niko on 2018/3/10.
+ */
+
+public class AdjustSize {
+
+    private DisplayMetrics dm;
+    private int screen_width;
+    private int screen_height;
+    private float density;
+
+    public AdjustSize(Context context){
+        Resources resources = context.getResources();
+
+        dm = resources.getDisplayMetrics();
+        density = dm.density;
+        screen_width = dm.widthPixels;
+        screen_height = dm.heightPixels;
+    }
+
+    public static Size getViewSize(View view){
+        /*
+        int width = View.MeasureSpec.makeMeasureSpec(0,
+                View.MeasureSpec.UNSPECIFIED);
+        int height = View.MeasureSpec.makeMeasureSpec(0,
+                View.MeasureSpec.UNSPECIFIED);
+        view.measure(width, height);
+
+        Size size = new Size();
+        size.width = view.getMeasuredWidth(); // 获取宽度
+        size.height = view.getMeasuredHeight(); // 获取高度
+*/
+        // 注意 这种获取 宽高的方式 只有当activity执行了 onWindowFocusChanged 之后才能准确获得.
+        Size size = new Size();
+        size.width = view.getWidth(); // 获取宽度
+        size.height = view.getHeight(); // 获取高度
+        return size;
+    }
+
+    public static Size getImageWidthHeight(Context context, int resId){
+        BitmapFactory.Options options = new BitmapFactory.Options();
+
+        /**
+         * 最关键在此，把options.inJustDecodeBounds = true;
+         * 这里再decodeFile()，返回的bitmap为空，但此时调用options.outHeight时，已经包含了图片的高了
+         */
+        options.inJustDecodeBounds = true;
+        Bitmap bitmap = BitmapFactory.decodeResource(context.getResources(), resId, options); // 此时返回的bitmap为null
+
+        /**
+         *options.outHeight为原始图片的高
+         */
+        Size size = new Size();
+        size.width = options.outWidth;
+        size.height = options.outHeight;
+        return size;
+    }
+
+    public static void adjustHeight(View v, Size originalSize){
+        Size currentSize = getViewSize(v);
+
+        ViewGroup.LayoutParams lp;
+        lp = v.getLayoutParams();
+        float h  = (float)currentSize.height * (float)originalSize.width / (float)originalSize.width;
+        lp.height = (int)h;
+    }
+
+
+}
---
+++ app/src/main/java/com/freecreator/whiteforest/ui/utils/Size.java
@@ -0,0 +1,10 @@
+package com.freecreator.whiteforest.ui.utils;
+
+/**
+ * Created by niko on 2018/3/10.
+ */
+
+public class Size {
+    public int width = 0;
+    public int height = 0;
+}
