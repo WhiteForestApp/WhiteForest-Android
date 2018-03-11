03c60287405609a5fa178b59bc2a01f2e99f61f5
NikoKVCS <kvcsgame@hotmail.com>
2018-03-11 18:12:57 +0800

    desire paghe

--- app/src/main/java/com/freecreator/whiteforest/ui/DesireActivity.java
+++ app/src/main/java/com/freecreator/whiteforest/ui/DesireActivity.java
@@ -1,13 +1,23 @@
 package com.freecreator.whiteforest.ui;
 
 import android.os.Bundle;
+import android.os.Debug;
 import android.support.v7.app.AppCompatActivity;
+import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.freecreator.whiteforest.R;
 import com.freecreator.whiteforest.ui.utils.AdjustSize;
+import com.freecreator.whiteforest.ui.utils.Size;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import static com.freecreator.whiteforest.common.Debug._Debug;
 
 /**
  * Created by niko on 2018/3/11.
@@ -36,11 +46,6 @@ public class DesireActivity extends AppCompatActivity {
         setContentView(R.layout.activity_desire);
 
         UI_init();
-        UI_setListeners();
-    }
-
-    private void UI_setListeners(){
-
     }
 
     private void UI_init() {
@@ -68,10 +73,93 @@ public class DesireActivity extends AppCompatActivity {
         AdjustSize.adjustHeight(ImageView_icon_desire, AdjustSize.getImageWidthHeight(this, R.drawable.desire));
     }
 
+    /**
+     * Why i use "JSONObject" as the param instead of params like "int type, String title, ..." ?
+     * Because in this way we can devide the UI part and the data part
+     * in long run, this kind of model (UI / Data part) will be convenient.
+     * @param position the position to insert a new item view
+     * @param data Json data. An sample is below:
+     *             {  "title" : "blablabla", "scores" : 6 }
+     */
+    private void UI_addItem(int position, JSONObject data){
+
+
+        FrameLayout item = (FrameLayout) DesireActivity.this.getLayoutInflater().inflate(R.layout.item_desire, null);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        String title = data.optString("title");
+        if(title != null && !title.equals("")){
+            int scores = data.optInt("scores");
+            title = "$" + scores + " " + title;
+            TextView text = (TextView)item.findViewById(R.id.text_title);
+            text.setText(title);
+        }
+
+        list_desire.addView(item, position, params);
+
+        LinearLayout space = (LinearLayout) DesireActivity.this.getLayoutInflater().inflate(R.layout.item_space, null);
+        Size list_desire_size = AdjustSize.getViewSize(list_desire);
+        Size refSize = new Size();
+        refSize.height = 8;
+        refSize.width = 500;
+        float h  = (float)list_desire_size.width * (float)refSize.height / (float)refSize.width;
+        params = new LinearLayout.LayoutParams(list_desire_size.width, (int)h);
+        list_desire.addView(space, position + 1, params);
+
+    }
+
     @Override
     public void onWindowFocusChanged (boolean hasFocus){
         super.onWindowFocusChanged(hasFocus);
 
         UI_adjust();
+
+        if(_Debug)
+            mockDataForTest();
     }
+
+
+    private void mockDataForTest(){
+
+        // { "type" : 1,  "title" : "blablabla", "scores" : 6, "exp" : 13}
+        JSONObject obj = new JSONObject();
+        try {
+
+            obj.put("scores", 6);
+            obj.put("title", "阿萨德阿萨德受到法律框架的疯狂攻击所发生的空间");
+            UI_addItem(0,obj);
+
+            obj.put("scores", 6);
+            obj.put("title", "阅读书籍");
+            UI_addItem(0,obj);
+
+            obj.put("scores", 6);
+            obj.put("title", "阿萨德阿萨德受到法律框架的疯狂攻击所发生的空间");
+            UI_addItem(0,obj);
+
+            obj.put("scores", 6);
+            obj.put("title", "阅读书籍");
+            UI_addItem(0,obj);
+
+            obj.put("scores", 6);
+            obj.put("title", "阿萨德阿萨德受到法律框架的疯狂攻击所发生的空间萨德阿萨德受到法律框架的疯狂攻击所发生的空间萨德阿萨德受到法律框架的疯狂攻击所发生的空间");
+            UI_addItem(0,obj);
+
+            obj.put("scores", 6);
+            obj.put("title", "阅读书籍");
+            UI_addItem(0,obj);
+
+            obj.put("scores", 6);
+            obj.put("title", "阿萨德阿萨德受到法律框架萨德阿萨德受到法律框架的疯狂攻击所发生的空间框架萨德阿萨德受到法律框架的疯狂攻击所发生的空间框架萨德阿萨德受到法律框架的疯狂攻击所发生的空间的疯狂攻击所发生的空间");
+            UI_addItem(0,obj);
+
+            obj.put("scores", 6);
+            obj.put("title", "阅读书籍");
+            UI_addItem(0,obj);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
