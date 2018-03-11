5f015d720d5964e17403a7e5793016bf1c7ea089
NikoKVCS <kvcsgame@hotmail.com>
2018-03-11 14:18:15 +0800

    new files

---
+++ app/src/main/java/com/freecreator/whiteforest/ui/utils/UIUtils.java
@@ -0,0 +1,43 @@
+package com.freecreator.whiteforest.ui.utils;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+
+/**
+ * Created by niko on 2018/3/11.
+ */
+
+public class UIUtils {
+
+    public static int px2dip(int pxValue)
+    {
+        final float scale = Resources.getSystem().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+
+    public static int dip2px(float dipValue)
+    {
+        final float scale = Resources.getSystem().getDisplayMetrics().density;
+        return  (int)(dipValue * scale + 0.5f);
+    }
+
+    public static int[] getScreenSize(Context context) {
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics outMetrics = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(outMetrics);
+        return new int[]{outMetrics.widthPixels, outMetrics.heightPixels};
+    }
+
+    public static void setMargins (View v, int l, int t, int r, int b) {
+        if (v.getLayoutParams() instanceof ViewGroup.MarginLayoutParams) {
+            ViewGroup.MarginLayoutParams p = (ViewGroup.MarginLayoutParams) v.getLayoutParams();
+            p.setMargins(l, t, r, b);
+            v.requestLayout();
+        }
+    }
+}
