553ec188c53e0e966f6586501a7ebc0cd1bb99a2
Cute Niko <Cute Niko>
2018-03-18 10:01:53 +0800

    登录页

---
+++ app/src/main/java/com/freecreator/whiteforest/ui/MainloginActivity.java
@@ -0,0 +1,21 @@
+package com.freecreator.whiteforest.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.freecreator.whiteforest.R;
+
+/**
+ * Created by niko on 2018/3/15.
+ */
+
+public class MainloginActivity  extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainlogin);
+    }
+
+}
--- app/src/main/java/com/freecreator/whiteforest/ui/StoryActivity.java
+++ app/src/main/java/com/freecreator/whiteforest/ui/StoryActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.freecreator.whiteforest.R;
 import com.freecreator.whiteforest.ui.utils.AdjustSize;
@@ -19,6 +20,10 @@ public class StoryActivity extends AppCompatActivity {
 
     private ImageView ImageView_story_header = null;
     private ImageView ImageView_story_bottom = null;
+
+    private TextView user_name = null;
+    private TextView user_name_2 = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +42,13 @@ public class StoryActivity extends AppCompatActivity {
 
         ImageView_story_header = (ImageView)findViewById(R.id.ImageView_story_header);
         ImageView_story_bottom = (ImageView)findViewById(R.id.ImageView_story_bottom);
+
+        user_name = (TextView)findViewById(R.id.user_name);
+        user_name_2 = (TextView)findViewById(R.id.user_name_2);
+
+        // 设置为用户名字
+        user_name.setText("灿柳");
+        user_name_2.setText("灿柳");
 
     }
 
