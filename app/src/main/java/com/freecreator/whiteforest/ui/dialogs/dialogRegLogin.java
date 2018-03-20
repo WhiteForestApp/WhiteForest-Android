c40e2bb253b91e19ada3bbd8aa5882c4d2274bda
Cute Niko <Cute Niko>
2018-03-20 11:19:59 +0800

    login

--- app/src/main/java/com/freecreator/whiteforest/ui/dialogs/dialogRegLogin.java
+++ app/src/main/java/com/freecreator/whiteforest/ui/dialogs/dialogRegLogin.java
@@ -1,11 +1,15 @@
 package com.freecreator.whiteforest.ui.dialogs;
 
-import android.app.Activity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 
 import com.freecreator.whiteforest.R;
+import com.freecreator.whiteforest.ui.fragments.fragmentLogin;
 
 /**
  * Created by niko on 2018/3/19.
@@ -14,10 +18,15 @@ import com.freecreator.whiteforest.R;
 public class dialogRegLogin {
 
 
-    private Activity m_parent = null;
+    private AppCompatActivity m_parent = null;
     private RelativeLayout mDialog = null;
 
-    public dialogRegLogin(Activity parent, RelativeLayout attachment){
+    private FragmentManager manager = null;
+    private FragmentTransaction transaction = null;
+
+    private fragmentLogin fragLogin = null;
+
+    public dialogRegLogin(AppCompatActivity parent, RelativeLayout attachment){
         m_parent = parent;
 
         mDialog = (RelativeLayout)m_parent.getLayoutInflater().inflate(R.layout.dialog_reglogin, null);
@@ -33,6 +42,17 @@ public class dialogRegLogin {
     }
 
     private void UI_init() {
+
+        fragLogin = new fragmentLogin();
+
+        manager = m_parent.getSupportFragmentManager();
+        transaction = manager.beginTransaction();
+        transaction.add(R.id.fragment, fragLogin);
+        transaction.commit();
+
+        transaction = manager.beginTransaction();
+        transaction.replace(R.id.fragment, fragLogin);
+        transaction.commit();
     }
 
     private void setListeners() {
