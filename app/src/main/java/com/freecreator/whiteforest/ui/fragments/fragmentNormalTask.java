838eb4871a7fcf05216b4619c769b4ed82a85060
kvcsgame@hotmail.com <kvcsgame@hotmail.com>
2018-03-29 11:19:23 +0800

    fragments

---
+++ app/src/main/java/com/freecreator/whiteforest/ui/fragments/fragmentNormalTask.java
@@ -0,0 +1,42 @@
+package com.freecreator.whiteforest.ui.fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.freecreator.whiteforest.R;
+
+/**
+ * Created by niko on 2018/3/29.
+ */
+
+public class fragmentNormalTask  extends Fragment {
+
+    private Context mContext = null;
+    private View fragmentView = null;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mContext = getActivity();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        fragmentView = inflater.inflate(R.layout.fragment_normal_task, container, false);
+        return fragmentView;
+    }
+
+
+    @Override
+    public View getView(){
+        return fragmentView;
+    }
+
+}
