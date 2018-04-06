024627852c3a4c7e40dd88a5939a6f2785a07d36
官宇霞 <1549624673@qq.com>
2018-04-07 07:48:14 +0800

    no message

--- app/src/main/java/com/freecreator/whiteforest/ui/dialogs/dialogAddDesire.java
+++ app/src/main/java/com/freecreator/whiteforest/ui/dialogs/dialogAddDesire.java
@@ -3,9 +3,12 @@ package com.freecreator.whiteforest.ui.dialogs;
 import android.app.Activity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.freecreator.whiteforest.R;
+import com.freecreator.whiteforest.ui.views.FontTextView;
 
 /**
  * Created by niko on 2018/3/12.
@@ -15,8 +18,13 @@ public class dialogAddDesire {
 
     private Activity m_parent = null;
     private RelativeLayout mDialog = null;
-
+    private EditText money=null;
+    private EditText exp=null;
+    private String s1 = null;
+    private String s2 = null;
+    private FontTextView right=null;
     public dialogAddDesire(Activity parent, RelativeLayout attachment){
+
         m_parent = parent;
 
         mDialog = (RelativeLayout)m_parent.getLayoutInflater().inflate(R.layout.dialog_add_desire_item, null);
@@ -32,6 +40,9 @@ public class dialogAddDesire {
     }
 
     private void UI_init() {
+        money=(EditText) mDialog.findViewById(R.id.btn_money);
+        exp = (EditText) mDialog.findViewById(R.id.exp_btn);
+        right = (FontTextView)mDialog.findViewById(R.id.fontTextView);
     }
 
     private void setListeners() {
@@ -39,6 +50,22 @@ public class dialogAddDesire {
             @Override
             public void onClick(View view) {
                 dialogAddDesire.this.hide();
+            }
+        });
+        right.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                s1 = money.getText().toString();
+                s2 = exp.getText().toString();
+                if("".equals(s1) || "".equals(s2)){
+                    Toast.makeText(m_parent,"kongzfc",Toast.LENGTH_SHORT).show();
+
+                }
+                else{
+                    money.setText("");
+                    exp.setText(" ");
+                    Toast.makeText(m_parent,"成功",Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
